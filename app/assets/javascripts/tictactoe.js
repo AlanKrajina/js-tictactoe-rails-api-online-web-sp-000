9f6ef1583e0b944485b4af1753fbbe3b7be5fa5f
Alan Krajina <alankrajina88@gmail.com>
2019-10-05 11:12:24 +0000

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -98,6 +98,35 @@ function saveGame(){
 // .toArray - retrieve all the elements contained in the jQuery set, as an array.
 // clicking the save button after loading the game sends patch request
 
+// https://github.com/learn-co-curriculum/js-tictactoe-rails-api/blob/solution/app/assets/javascripts/tictactoe.js
+
+//function saveGame() {
+//  var state = [];
+//  var gameData;
+
+//  $('td').text((index, square) => {
+//    state.push(square);
+//  });
+
+//  gameData = { state: state };
+
+//  if (currentGame) {
+//    $.ajax({
+//      type: 'PATCH',
+//      url: `/games/${currentGame}`,
+//      data: gameData
+//    });
+//  } else {
+//    $.post('/games', gameData, function(game) {
+//      currentGame = game.data.id;
+//      $('#games').append(`<button id="gameid-${game.data.id}">${game.data.id}</button><br>`);
+//      $("#gameid-" + game.data.id).on('click', () => reloadGame(game.data.id));
+//    });
+//  }
+//}
+
+
+
 function clearGame(){
  $('td').empty();
  turn = 0;
